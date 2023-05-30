52e7ecb709839f7ec0fd8e60dae67a29a767cca9
Minwookim <trte135@naver.com>
2023-05-30 11:06:45 +0900

    fix. May.30.2023 Java [Refactoring]  - Content :    - 윤년구하기 [checkLeapYear]

--- likelion/src/main/java/org/example/Main.java
+++ likelion/src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    checkLeapYear();
+  }
+
+  // Refactoring
+  private static void checkLeapYear() {
+    Scanner sc = new Scanner(System.in);
+
+    int year = sc.nextInt();
+
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+      System.out.printf("%d O \n", year);
+    } else {
+      System.out.printf("%d X \n", year);
+    }
   }
 }
