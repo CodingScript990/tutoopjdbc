85e2b29dac0c5308e99aebcabce44e9c4db1508b
trte1 <trte135@naver.com>
2023-06-01 10:07:45 +0900

    fix. Java - [게임규칙을 인터페이스로 분리 완료]  - Content :    - OpGame[Interface 상속 받아 사용되게 함]    - PlusOperatorLevelOne[IOperator interface를 상속 받아서 각 메서드를 통해서 게임이 진행되도록 만듬]

--- likelion/src/main/java/org/example/opgame/service/OpGame.java
+++ likelion/src/main/java/org/example/opgame/service/OpGame.java
@@ -2,12 +2,12 @@ package org.example.opgame.service;
 
 public class OpGame {
     // Member Field
-    int inputNumOne, inputNumTwo; // 출제 값1,2
+
     int remainingAnswers; // 3번의 기회제공
     String cheeringUpMsg = "한번 더 해보자!"; // message
     IOperator op; // interface value
 
-    //
+    // IOperator 를 사용하기 위한 메서드(Interface)
     public OpGame(IOperator op) {
         this.op = op;
     }
@@ -16,22 +16,17 @@ public class OpGame {
     public void makeQuestion() {
         remainingAnswers = 3; // 3번의 기회 제공
 
-        //inputNumOne = 1; // 출제 값1
-        //inputNumTwo = 1; // 출제 값2
         op.setA(1); // 값1를 담아주는 역할
         op.setB(1); // 값2를 담아주는 역할
     }
 
     // 랜덤 메서드
     public void makeQuestion(int max) {
-        //inputNumOne = (int) (Math.random() * max); // 출제 값1
-        //inputNumTwo = (int) (Math.random() * (max - inputNumOne)); // 출제 값2
         op.generateQuestion(max); // max 값이 반환되도록 함
     }
 
     // 문제 메서드
     public String getQuestion() {
-        //return String.format("%d + %d = ", inputNumOne, inputNumTwo);
         return op.getQuestionMsg(); // message 값이 반환되도록 함
     }
 
@@ -43,7 +38,6 @@ public class OpGame {
     // 정답이 아닐 시 기회를 감소시키는 메서드
     public boolean isAnswer(int answer) {
         remainingAnswers--;
-        // return (inputNumOne + inputNumTwo) == answer;
         return op.isEquals(answer); // 값을 비교하고 반환함
     }
 
--- likelion/src/main/java/org/example/opgame/service/PlusOperatorLevelOne.java
+++ likelion/src/main/java/org/example/opgame/service/PlusOperatorLevelOne.java
@@ -3,28 +3,31 @@ package org.example.opgame.service;
 // IOperator interface 를 상속 받아줌
 // 새로운 + 게임을 만들고 OpGame Class 에서 요구하는 방식만 넣어주도록 해주기 위함
 public class PlusOperatorLevelOne implements IOperator{
+    // Member Field
+    int inputNumOne, inputNumTwo; // 출제 값1,2
     @Override
     public void setA(int inputNumOne) {
-        
+        this.inputNumOne = inputNumOne;
     }
 
     @Override
     public void setB(int inputNumTwo) {
-
+        this.inputNumTwo = inputNumTwo;
     }
 
     @Override
     public void generateQuestion(int max) {
-
+        inputNumOne = (int) (Math.random() * max); // 출제 값1
+        inputNumTwo = (int) (Math.random() * (max - inputNumOne)); // 출제 값2
     }
 
     @Override
     public String getQuestionMsg() {
-        return null;
+        return String.format("%d + %d = ", inputNumOne, inputNumTwo);
     }
 
     @Override
     public boolean isEquals(int answer) {
-        return false;
+         return (inputNumOne + inputNumTwo) == answer;
     }
 }
